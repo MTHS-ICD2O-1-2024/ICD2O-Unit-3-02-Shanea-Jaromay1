62ce67bb5636a9aec38aa93af905255e45871c43
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-03-31 17:31:03 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume of a pyramid.
+ */
+function calculateVolumeOfPyramid () {
+  // input
+  const lengthOfPyramid = parseInt(document.getElementById('length-of-pyramid').value)
+  const widthOfPyramid = parseFloat(document.getElementById('width-of-pyramid').value)
+  const heightOfPyramid = parseInt(document.getElementById('height-of-pyramid').value)
+
+  // process
+  const volumeOfPyramid = ( lengthOfPyramid * widthOfPyramid * heightOfPyramid ) / 3
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volumeOfPyramid + ' mm³'
+}
+
